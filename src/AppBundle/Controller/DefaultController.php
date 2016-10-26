ade98ed48b8d1a15f7067877923fb3fa7496156a
bhdm <bhd.m@ya.ru>
2016-10-26 16:53:19 +0300

    style uptdaes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,6 +14,6 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->redirectToRoute('company_list');
+        return $this->redirectToRoute('mediaplan_list');
     }
 }
